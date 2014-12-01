eebc072ba80c05cdc1335b4e2ff138addd6dde61
Kagami Hiiragi <kagami@genshiken.org>
2014-12-01 23:48:30 +0300

    webpack-dev-server stub

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 var path = require("path");
 var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 var DEBUG = process.env.NODE_ENV !== "production";
 
@@ -9,14 +10,31 @@ function q(loaders, query) {
   return loaders + "?" + JSON.stringify(query);
 }
 
+function st(filename) {
+  return path.join("static", filename);
+}
+
+function getNameBySuffix(suffix, stats) {
+  return stats.assets.filter(function(asset) {
+    return asset.name.slice(-suffix.length) === suffix;
+  })[0].name;
+}
+
+var commonPlugins = [
+  new HtmlWebpackPlugin({
+    template: path.join("src", "index.html"),
+    bySuffix: getNameBySuffix,
+  }),
+];
+
 module.exports = {
   entry: {
-    nekogrid: "./src/nekogrid",
     vendor: "./src/vendor",
+    nekogrid: "./src/nekogrid",
   },
   output: {
-    path: path.join(__dirname, "dist", "static"),
-    filename: DEBUG ? "[name].js" : "[chunkhash:15].[name].js",
+    path: path.join(__dirname, "dist"),
+    filename: st(DEBUG ? "[name].js" : "[chunkhash:15].[name].js"),
   },
 
   module: {
@@ -24,19 +42,25 @@ module.exports = {
       // All JS sources except vendor.
       {
         test: new RegExp("^" + path.join(__dirname, "src", "(?!vendor).+\\.js$")),
-        loader: "traceur",
+        loader: q("traceur", {
+          // Don't use sourcemaps at this stage.
+          sourceMaps: false,
+        }),
       },
       // Vendor javascript.
       {
         test: path.join(__dirname, "src", "vendor.js"),
-        loader: "traceur?runtime",
+        loader: q("traceur", {
+          runtime: true,
+          sourceMaps: false,
+        }),
       },
       // Vendor SCSS.
       {
         test: /\.scss$/,
         loaders: [
           q("file", {
-            name: DEBUG ? "[name].css" : "[hash:15].[name].css",
+            name: st(DEBUG ? "[name].css" : "[hash:15].[name].css"),
           }),
           q("sass", {
             outputStyle: DEBUG ? "expanded" : "compressed",
@@ -47,9 +71,9 @@ module.exports = {
     ],
   },
 
-  plugins: DEBUG ? [] : [
+  plugins: DEBUG ? commonPlugins : commonPlugins.concat([
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-  ],
+  ]),
 
 };
